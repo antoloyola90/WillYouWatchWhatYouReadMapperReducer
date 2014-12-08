9ef056b001e2bc494348b0f455f711ef4d0b1f5d
rachitahajela <rachitahajela.25@gmail.com>
2014-12-07 19:18:53 -0800

    pig commands

--- src/MovieAdaptationsMapper.java
+++ src/MovieAdaptationsMapper.java
@@ -6,60 +6,53 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class MovieAdaptationsMapper extends Mapper<LongWritable, Text, Text, Text> {
-	
-	
+public class MovieAdaptationsMapper extends
+		Mapper<LongWritable, Text, Text, Text> {
 
-  @Override
-  public void map(LongWritable key, Text value, Context context)
-      throws IOException, InterruptedException {
-	  try{
-	  String line = value.toString();
-	 
-	  System.out.println(line);
-	  String ImdbRating = line.substring(line.indexOf("redoneMovieRating"));
-	  ImdbRating = ImdbRating.split(",")[0];
-	  ImdbRating = ImdbRating.split(":")[1].trim();
-	  ImdbRating = ImdbRating.split("[(]")[0];
-	  //ImdbRating = ImdbRating.substring(1, ImdbRating.length()-1);
-	  System.out.println(ImdbRating);
-	//  Thread.sleep(5000);
-	  
-	  String Genre = line.substring(line.indexOf("Genre"));
-	  Genre = Genre.split(",")[0];
-	  Genre = Genre.split(":")[1].trim();
-	  System.out.println(Genre);
-	  Genre = Genre.replace("###"," ");
-	  
-	  StringTokenizer tokenizer=new StringTokenizer(Genre);
-	  int count=tokenizer.countTokens();
-	  System.out.println(Genre);
-	  System.out.println(count);
-	  
-	  float rank = 0;
-	   
-	 if((Float.parseFloat(ImdbRating))>=7)
-	 {
-		 rank = (float) 1/ (float) count;
-		 System.out.println(rank);
-	 }
-	  
-	 while(tokenizer.hasMoreTokens())
-	  {
-		  String word=tokenizer.nextToken();
-		 
-		  
-			  context.write(new Text(word), new Text(rank +""));
-			  System.out.println(word + " "+ rank);
-			 
-		  	  }
-	  
-	  
-	 
-			  
-		  
-	  }catch(Exception e) {
-		  
-	  }
-  }
+	@Override
+	public void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		try {
+			String line = value.toString();
+
+			System.out.println(line);
+			String ImdbRating = line.substring(line
+					.indexOf("redoneMovieRating"));
+			ImdbRating = ImdbRating.split(",")[0];
+			ImdbRating = ImdbRating.split(":")[1].trim();
+			ImdbRating = ImdbRating.split("[(]")[0];
+			// ImdbRating = ImdbRating.substring(1, ImdbRating.length()-1);
+			System.out.println(ImdbRating);
+			// Thread.sleep(5000);
+
+			String Genre = line.substring(line.indexOf("Genre"));
+			Genre = Genre.split(",")[0];
+			Genre = Genre.split(":")[1].trim();
+			System.out.println(Genre);
+			Genre = Genre.replace("###", " ");
+
+			StringTokenizer tokenizer = new StringTokenizer(Genre);
+			int count = tokenizer.countTokens();
+			System.out.println(Genre);
+			System.out.println(count);
+
+			float rank = 0;
+
+			if ((Float.parseFloat(ImdbRating)) >= 7) {
+				rank = 1;
+				// System.out.println(rank);
+			}
+
+			while (tokenizer.hasMoreTokens()) {
+				String word = tokenizer.nextToken();
+
+				context.write(new Text(word), new Text(rank + ""));
+				System.out.println(word + " " + rank);
+
+			}
+
+		} catch (Exception e) {
+
+		}
+	}
 }
--- src/MovieAdaptationsReducer.java
+++ src/MovieAdaptationsReducer.java
@@ -6,15 +6,19 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class MovieAdaptationsReducer extends Reducer<Text, Text, Text, Text> {
 
-  @Override
-  public void reduce(Text key, Iterable<Text> values, Context context)
-      throws IOException, InterruptedException {
-	  Double temp;
-	  Double rank = 0.0;
-	  for (Text value : values) {
-		 temp = Double.parseDouble(value.toString());
-		  rank = rank + temp ;
-	  }
-context.write(key, new Text(rank+""));
-  }
+	@Override
+	public void reduce(Text key, Iterable<Text> values, Context context)
+			throws IOException, InterruptedException {
+		float temp;
+		float rank = 0;
+		long count = 0;
+		for (Text value : values) {
+			temp = Float.parseFloat(value.toString());
+			rank = rank + temp;
+			count++;
+		}
+		rank = rank/count;
+		rank = rank * 100;
+		context.write(key, new Text(rank + ""+" "+count));
+	}
 }
